a6460d2e89b1d097d4ee72ef997407988892eb4c
lassehj0 <lassehj00@github.com>
2022-10-18 17:47:56 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'c:/Users/Lasse/OneDrive/Dokumenter/Programming/Dinner-app/Dinner-app/Dinner-app/dinner-app/node_modules/axios/index.js';
+import cheerio from 'cheerio';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const AxiosInstance = axios.create();
+	const [names, setNames] = useState<string[]>(() => []);
+	const [url, setUrl] = useState('https://www.valdemarsro.dk/opskrifter/');
+
+	useEffect(() => {
+		console.log(url);
+		AxiosInstance.get(url)
+			.then((response) => {
+				const html = response.data;
+				const $ = cheerio.load(html);
+				const statsTable = $('.post-list-item-title > a');
+				const nextPageLink = $('.pagenav-new > div > a');
+
+				statsTable.each((i, elem) => {
+					var name: string = $(elem).text();
+					const index: number = name.indexOf('-');
+					name = index !== -1 ? name.substring(0, index) : name;
+					setNames((prevNames) => [...prevNames, name]);
+				});
+
+				if (nextPageLink)
+					nextPageLink.each((i, elem) => {
+						if ($(elem).text().indexOf('Næste') !== -1)
+							setUrl($(elem).attr('href')!);
+					});
+			})
+			.catch(console.error);
+	}, [url]);
+
+	return (
+		<>
+			<ul className='list'>
+				{names.map((element, index) => {
+					return (
+						<div className='list-item' id={'Recipe nr.' + index} key={index}>
+							<li>{element}</li>
+						</div>
+					);
+				})}
+			</ul>
+		</>
+	);
 }
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="react-scripts" />
+GENERATE_SOURCEMAP = false;
